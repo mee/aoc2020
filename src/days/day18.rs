e321b6125a20c499735b9a6f0f1e699e3eaef044
Mike Erickson <mike.erickson@gmail.com>
2021-01-01 21:19:04 -0500

    day 18 part 1

--- src/days/day18.rs
+++ src/days/day18.rs
@@ -1,8 +1,16 @@
 use std::error::Error;
 use std::str::FromStr;
-pub fn day18() {}
+pub fn day18() {
+    let mut sum = 0;
+    for line in include_str!("18.input").lines() {
+        let res = eval_str(line).unwrap();
+        println!("{:>12} = {}", res, line);
+        sum += res;
+    }
+    println!("The sum of all results is {}", sum);
+}
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct EvalError;
 #[derive(Debug)]
 struct OpParseError;
@@ -22,7 +30,7 @@ impl std::fmt::Display for OpParseError {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Op {
     Add,
     Multiply,
@@ -30,7 +38,6 @@ enum Op {
 
 impl Op {
     fn apply(self: &Self, a: isize, b: isize) -> isize {
-        eprintln!("Performing {:?} on {}, {}", self, a, b);
         match self {
             Op::Add => a + b,
             Op::Multiply => a * b,
@@ -42,112 +49,66 @@ impl FromStr for Op {
     type Err = OpParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(match s {
-            "*" => Op::Multiply,
-            "+" => Op::Add,
-            _ => panic!("ahhhhh! '{}'", s),
-        })
+        match s {
+            "*" => Ok(Op::Multiply),
+            "+" => Ok(Op::Add),
+            _ => Err(OpParseError),
+        }
     }
 }
 
-fn eval_str(s: &str) -> Result<isize, EvalError> {
-    // holy "unwrap", batman
-    if let Some(pi_lh) = s.find('(') {
-        // parse expr + op on lhs of (, op + expr on rhs of ) and return
+fn find_closing_paren(s: &str, pi_lh: usize) -> usize {
+    s[pi_lh..]
+        .chars()
+        .scan(0, |pc, c| {
+            *pc = match c {
+                '(' => *pc + 1,
+                ')' => *pc - 1,
+                _ => *pc,
+            };
+            Some(*pc)
+        })
+        .take_while(|&e| e > 0)
+        .collect::<Vec<usize>>()
+        .len()
+        + pi_lh
+}
 
-        // does rust have scanl?
-        let pi_rh = {
-            let mut pc: isize = 1;
-            let mut ci = pi_lh + 1;
-            loop {
-                let c = &s[ci..ci + 1];
-                pc += match c {
-                    "(" => 1,
-                    ")" => -1,
-                    _ => 0,
-                };
-                if pc == 0 {
-                    break ci;
-                } else if ci == s.len() - 1 {
-                    panic!("no closing paren found");
-                }
-                ci += 1;
-            }
-        };
-
-        dbg!(s, pi_lh, pi_rh);
-
-        // is there an lhs expression? lhs op ( in ) ?
-        if pi_lh > 0 {
-            // is there a rhs expression?
-            if pi_rh < s.len() - 1 {
-                // lhs op ( in ) op rhs
-                let expr_lh = &s[0..pi_lh - 3];
-                let expr_in = &s[pi_lh + 1..pi_rh];
-                let expr_rh = &s[pi_rh + 4..];
-                let op_lh = &s[pi_lh - 2..pi_lh - 1]
-                    .parse::<Op>()
-                    .map_err(|_| EvalError)?;
-                let op_rh = &s[pi_rh + 2..pi_rh + 3]
-                    .parse::<Op>()
-                    .map_err(|_| EvalError)?;
-
-                return Ok(op_rh.apply(
-                    op_lh.apply(eval_str(expr_lh)?, eval_str(expr_in)?),
-                    eval_str(expr_rh)?,
-                ));
-            } else {
-                // lhs op ( in )
-                let expr_lh = &s[0..pi_lh - 3];
-                let expr_in = &s[pi_lh + 1..pi_rh];
-                let op_lh = &s[pi_lh - 2..pi_lh - 1]
-                    .parse::<Op>()
-                    .map_err(|_| EvalError)?;
-
-                return Ok(op_lh.apply(eval_str(expr_lh)?, eval_str(expr_in)?));
-            }
+fn get_next_expr(s: &str) -> Result<(isize, &str), EvalError> {
+    if &s[0..=0] == "(" {
+        let pi_rh = find_closing_paren(s, 0);
+        let (expr_in, remain) = if pi_rh < s.len() - 1 {
+            (&s[1..pi_rh], &s[pi_rh + 1..])
         } else {
-            // is there an rhs expression?
-            if pi_rh < s.len() - 1 {
-                // ( in ) op_rh rhs
-                let expr_in = &s[pi_lh + 1..pi_rh];
-                let expr_rh = &s[pi_rh + 4..];
-                let op_rh = &s[pi_rh + 2..pi_rh + 3]
-                    .parse::<Op>()
-                    .map_err(|_| EvalError)?;
-
-                return Ok(op_rh.apply(eval_str(expr_in)?, eval_str(expr_rh)?));
-            } else {
-                // ( in )
-                let expr_in = &s[pi_lh + 1..pi_rh];
-                return Ok(eval_str(expr_in)?);
-            }
-        }
+            (&s[1..s.len() - 1], "")
+        };
+        return eval_str(expr_in).map(|v| (v, remain));
     } else {
-        let mut parts = s.split(' ').collect::<Vec<&str>>();
-        println!("Parsing final reduced expression: {:?}", &parts);
-        parts.reverse();
-        let mut res: isize = parts
-            .pop()
-            .ok_or_else(|| EvalError)?
+        let val_str = s.chars().take_while(|&c| c != ' ').collect::<String>();
+        return val_str
             .parse::<isize>()
-            .map_err(|_| EvalError)?;
-
-        while parts.len() >= 2 {
-            let op = parts
-                .pop()
-                .ok_or_else(|| EvalError)?
-                .parse::<Op>()
-                .map_err(|_| EvalError)?;
-            let rhs = parts
-                .pop()
-                .ok_or_else(|| EvalError)?
-                .parse::<isize>()
-                .map_err(|_| EvalError)?;
-            res = op.apply(res, rhs);
-        }
-        return Ok(res);
+            .map_err(|_| EvalError)
+            .map(|v| (v, &s[val_str.len()..]));
     }
+}
+
+fn get_next_op(s: &str) -> Result<(Op, &str), EvalError> {
+    s[1..=1]
+        .parse::<Op>()
+        .map_err(|_| EvalError)
+        .map(|op| (op, &s[3..]))
+}
+
+fn eval_str(s: &str) -> Result<isize, EvalError> {
+    let (mut cur, mut s) = get_next_expr(s)?;
+    while !s.is_empty() {
+        let (op, ns) = get_next_op(s)?;
+        let (rh, ns) = get_next_expr(ns)?;
+        cur = op.apply(cur, rh);
+        s = ns;
+    }
+
+    Ok(cur)
 }
 
 #[cfg(test)]
@@ -155,7 +116,42 @@ mod test {
     use super::*;
     #[test]
     fn test_parse_failure() {
-        assert_eq!(eval_str("x + 2 * 3 + 4 * 5 + 6").unwrap(), 71);
+        assert_eq!(eval_str("x + 2 * 3 + 4 * 5 + 6"), Err(EvalError));
+    }
+
+    #[test]
+    fn test_get_next_op() {
+        assert_eq!(get_next_op(" + 3").unwrap(), (Op::Add, "3"));
+    }
+
+    #[test]
+    fn test_find_closing_paren() {
+        assert_eq!(find_closing_paren("((a)bb())", 0), 8);
+    }
+
+    #[test]
+    fn test_get_next_expr1() {
+        assert_eq!(get_next_expr("1").unwrap(), (1, ""));
+    }
+
+    #[test]
+    fn test_get_next_expr2() {
+        assert_eq!(get_next_expr("1 + 2 + 3").unwrap(), (1, " + 2 + 3"));
+    }
+
+    #[test]
+    fn test_get_next_expr3() {
+        assert_eq!(get_next_expr("1 + (2 + 3)").unwrap(), (1, " + (2 + 3)"));
+    }
+
+    #[test]
+    fn test_get_next_expr4() {
+        assert_eq!(get_next_expr("(1 + 2) + 3").unwrap(), (3, " + 3"));
+    }
+
+    #[test]
+    fn test_get_next_expr5() {
+        assert_eq!(get_next_expr("(1 + 2)").unwrap(), (3, ""));
     }
 
     #[test]
